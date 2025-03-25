ae784876970b93ab21cf651cd324317da3657e8d
BastienPeyre <55595986+BastienPeyre@users.noreply.github.com>
2025-03-25 21:56:45 +0100

    Ajout des URLs principales de l'application

---
+++ ocr_ws/urls.py
@@ -0,0 +1,16 @@
+"""
+URL configuration for ocr_ws project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('ocr_api.urls')),
+]
+
+# Servir les fichiers media en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
